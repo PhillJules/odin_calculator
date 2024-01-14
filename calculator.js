645a5162d1da6b3a75db25a704d943c3836e5e8f
Phillipe John Jules <phillipejohnjules@gmail.com>
2024-01-14 18:24:34 +0000

    finished checking functions
    all of the functions are working properly

--- calculator.js
+++ calculator.js
@@ -1,15 +1,19 @@
 function addition(a, b){
   return a + b;
 }
+console.log(addition(5, 2));
 
 function subtraction(a, b){
   return a - b;
 }
+console.log(subtraction(5, 2));
 
 function multiplication(a, b){
   return a * b;
 }
+console.log(multiplication(5, 2));
 
 function division(a, b){
   return a / b;
 }
+console.log(division(10, 2));
