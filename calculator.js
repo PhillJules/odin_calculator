1205521967ad67f55f5cf52c75fca834e3792b19
Phillipe John Jules <phillipejohnjules@gmail.com>
2024-01-14 23:53:32 +0000

    event listener is now active on the buttons
    all the buttons now have a value showing on the screen  when clicked. now
    i need to add the number to a variable.

--- calculator.js
+++ calculator.js
@@ -1,27 +1,34 @@
 let num1 = null;
 let num2 = null;
-let operator = null;
+let operator = document.getElementsByClassName("operator");
+const display = document.getElementById("display");
+let buttons = document.querySelectorAll('.integer, .operator');
+// let integer = document.getElementsByClassName("integer");
+
+buttons.forEach(button => {
+  button.addEventListener('click', function () {
+    updateDisplay(button.textContent);
+    if (button.classList.contains("operator")){
+      updateDisplay(button.textContent);
+    }
+  });
+});
 
 function addition(a, b){
   return a + b;
 }
-console.log(addition(5, 2));
 
 function subtraction(a, b){
   return a - b;
 }
-console.log(subtraction(5, 2));
 
 function multiplication(a, b){
   return a * b;
 }
-console.log(multiplication(5, 2));
 
 function division(a, b){
   return a / b;
 }
-console.log(division(10, 2));
-
 
 function operate(operator, num1, num2){
   if (operator === "+"){
@@ -34,4 +41,9 @@ function operate(operator, num1, num2){
   division(num1, num2);
 }
 
+}
+
+function updateDisplay(value){
+  display.value = display.value === "0" ? value : display.value + value;
+  console.log("button clicked");
 }
