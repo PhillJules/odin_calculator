a4f9e879ced3caf31b0ad829936307bbb2f06ace
Phillipe John Jules <phillipejohnjules@gmail.com>
2024-01-14 18:39:53 +0000

    added new function
    added the new function operate. this new function will take an operator
    and 2 numbers and then call one of the above functions on the numbers. Also
    added three new variables to store the numbers and operator.

--- calculator.js
+++ calculator.js
@@ -1,3 +1,7 @@
+let num1 = null;
+let num2 = null;
+let operator = null;
+
 function addition(a, b){
   return a + b;
 }
@@ -17,3 +21,17 @@ function division(a, b){
   return a / b;
 }
 console.log(division(10, 2));
+
+
+function operate(operator, num1, num2){
+  if (operator === "+"){
+    addition(num1, num2);
+} else if (operator === "-"){
+  subtraction(num1, num2);
+} else if (operator === "*"){
+  multiplication(num1, num2);
+} else if (operator === "/"){
+  division(num1, num2);
+}
+
+}
